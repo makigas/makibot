d50b8c9cd8927c22be2674a15fbaeb2df0d9aeac
Dani Rodríguez <dani@danirod.es>
2017-10-28 21:41:22 +0200

    !prime and !horn commands
    Oh this looks ugly

--- src/Makibot.js
+++ src/Makibot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const isPrime = require('is-prime');
 
 class Makibot extends Discord.Client {
 
@@ -23,6 +24,29 @@ class Makibot extends Discord.Client {
     console.log(`${msg.author.tag} said at ${msg.channel.name}: ${msg.content}`);
     if (msg.content == '!ping') {
       msg.reply('pong!');
+    }
+
+    let prime = /^!prime (\d+)$/;
+    if (prime.test(msg.content)) {
+      let input = parseInt(prime.exec(msg.content)[1])
+      if (isPrime(input)) {
+        msg.reply(`Se da la circunstancia de que sí, ${input} es primo.`);
+      } else if (input % 2 == 0) {
+        msg.reply(`amigo, deberías saber que un par no puede ser primo.`);
+      } else {
+        msg.reply(`No, ${input} no es primo.`);
+      }
+    }
+
+    if (msg.content == '!horn') {
+      if (msg.member.voiceChannel) {
+        let channel = msg.member.voiceChannel;
+        channel.join().then(conn => {
+          let dispatcher = conn.playFile('contrib/horn.mp3');
+          dispatcher.on('end', () => conn.disconnect());
+          dispatcher.on('error', e => console.log(e));
+        }).catch(console.log);
+      }
     }
   }
 
