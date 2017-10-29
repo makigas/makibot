cd459540fa559c6b9bc6af6331e4bd7628f921f8
Dani Rodríguez <dani@danirod.es>
2017-10-29 18:05:45 +0100

    Split the commands in files
    This commit moves the different commands already added to Makibot.js
    into subcommands in the commands directory.

---
+++ src/commands/utiles/ping.js
@@ -0,0 +1,16 @@
+const Commando = require('discord.js-commando');
+
+module.exports = class PingCommand extends Commando.Command {
+    constructor(client) {
+      super(client, {
+        name: 'ping',
+        memberName: 'ping',
+        group: 'utiles',
+        description: 'Determina si el bot está vivo'
+      });
+    }
+
+    async run(msg, argv) {
+      msg.reply('pong');
+    }
+  }
