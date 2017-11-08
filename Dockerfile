d83d652f05d69fa80f9aa93e522a112b47da6d15
Dani Rodríguez <dani@danirod.es>
2017-11-08 22:53:10 +0100

    Adds an initial Dockerfile to build a Docker image

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+# CLANK DOCKERFILE 🤖
+
+# Alpine Linux because I can only afford slim images.
+FROM node:8.9.0-alpine
+
+# Please, use the issue tracker instead of this.
+LABEL maintainer="dani@danirod.es"
+
+# Installs ffmpeg for being able to talk in voice channels.
+# Python and Alpine SDK required because of node-opus – also related to voice.
+# Git is required to fetch some unstable dependencies at the moment.
+RUN apk add --no-cache --update alpine-sdk ffmpeg git python
+
+# Let's get this started.
+RUN mkdir /clank
+WORKDIR /clank
+
+# Install dependencies
+ADD package.json package.json
+ADD package-lock.json package-lock.json
+RUN npm install
+
+# Not so fast, kid.
+USER node
+
+# Install remaining files
+ADD . .
+
+# So Node.js has a hard time running an application as PID 1. To catch
+# SIGTERM and SIGINT and gracefully log out the bot, our entrypoint here
+# is an init process whose only responsability is to start npm and catch
+# our signals just to forward them to the script.
+RUN wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64
+RUN chmod +x /usr/local/bin/dumb-init
+ENTRYPOINT ["/usr/local/bin/dumb-init", "--"]
+CMD ["npm", "start"]
--- src/Makibot.ts
+++ src/Makibot.ts
@@ -1,10 +1,12 @@
 import * as Commando from 'discord.js-commando';
 import * as sqlite from 'sqlite';
 import * as path from 'path';
+import * as fs from 'fs';
 import ConfigSchema from './ConfigSchema';
 import { Message } from 'discord.js';
 import PinService from './hooks/pin';
 import { config as XDG_CONFIG } from 'xdg-basedir';
+import * as mkdirp from 'mkdirp';
 
 export default class Makibot extends Commando.CommandoClient {
 
@@ -31,7 +33,8 @@ export default class Makibot extends Commando.CommandoClient {
             console.log(`Logged in successfully as ${this.user.tag}.`);
 
             // Load persistent settings.
-            sqlite.open(path.join(XDG_CONFIG, 'clank', 'settings.db'))
+            this.settingsFile()
+                .then(file => sqlite.open(file))
                 .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
                 .then(() => this.onDatabaseReady())
                 .catch(console.log);
@@ -52,6 +55,26 @@ export default class Makibot extends Commando.CommandoClient {
         this.user.setPresence({
             status: this.settings.get('BotPresence', 'online'),
             game: { name: this.settings.get('BotActivity', null) }
+        });
+    }
+
+    private settingsFile(): Promise<string> {
+        // This is what we want to return.
+        let configFile = path.join(XDG_CONFIG, 'clank', 'settings.db');
+
+        // But, hold on! Because it may be that the parent directory does not exist.
+        let configDir = path.dirname(configFile);
+        return new Promise((resolve, reject) => {
+            if (!fs.existsSync(configDir)) {
+                // I told you! Let's fix this.
+                mkdirp(configDir, err => {
+                    if (err) reject(err);
+                    else resolve(configFile);
+                });
+            } else {
+                // Ok, just let it go.
+                resolve(configFile);
+            }
         });
     }
 
