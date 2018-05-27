0d199793ab0ed767503c7ad2b67537fb0b45e2c9
Dani Rodríguez <dani@danirod.es>
2018-05-27 18:21:09 +0200

    Show status when !helper is sent with no arguments

--- src/commands/utiles/helper.ts
+++ src/commands/utiles/helper.ts
@@ -2,7 +2,7 @@ import * as Commando from 'discord.js-commando';
 import { Client, Guild, Role } from 'discord.js';
 
 interface HelperCommandArguments {
-    mode: boolean
+    mode: string
 }
 
 export = class HelperCommand extends Commando.Command {
@@ -15,7 +15,7 @@ export = class HelperCommand extends Commando.Command {
             description: 'Te mete o te quita del rol de helpers.',
             examples: ['helper true', 'helper false'],
             args: [
-                { key: 'mode', type: 'boolean', prompt: '¿Quieres entrar o salir del rol helpers?.', default: '' }
+                { key: 'mode', type: 'string', prompt: '¿Quieres entrar o salir del rol helpers?.', default: '' }
             ]
         });
     }
@@ -34,24 +34,63 @@ export = class HelperCommand extends Commando.Command {
         let role = msg.guild.roles.find('name', roleName);
 
         // Act on behalf of what the user wants.
-        if (args.mode) {
-            // Add the user to this role unless they're already in.
-            if (msg.member.roles.has(role.id)) {
-                return msg.reply(`Ya formabas parte del rol ${roleName}.`);
-            } else {
-                return msg.member.addRole(role)
-                    .then(member => msg.reply(`Ahora estás en el rol ${roleName}.`))
-                    .catch(e => msg.reply(`No puedo satisfacer tu orden porque ${e}.`));
-            }
-        } else {
-            // Remove the user from this role unless they're never in.
-            if (msg.member.roles.has(role.id)) {
-                return msg.member.removeRole(role)
-                    .then(member => msg.reply(`Ya no formas parte del rol ${roleName}.`))
-                    .catch(e => msg.reply(`No puedo satisfacer tu orden porque ${e}.`));
-            } else {
-                return msg.reply(`Nunca has formado parte del rol ${roleName}.`);
-            }
+        switch (HelperCommand.opmode(args.mode)) {
+            case 'yes':
+                if (msg.member.roles.has(role.id)) {
+                    return msg.reply(`Ya formabas parte del rol ${roleName}.`);
+                } else {
+                    return msg.member.addRole(role)
+                        .then(member => msg.reply(`Ahora estás en el rol ${roleName}.`))
+                        .catch(e => msg.reply(`No puedo satisfacer tu orden porque ${e}.`));
+                }
+
+            case 'no':
+                // Remove the user from this role unless they're never in.
+                if (msg.member.roles.has(role.id)) {
+                    return msg.member.removeRole(role)
+                        .then(member => msg.reply(`Ya no formas parte del rol ${roleName}.`))
+                        .catch(e => msg.reply(`No puedo satisfacer tu orden porque ${e}.`));
+                } else {
+                    return msg.reply(`No formabas parte del rol ${roleName}.`);
+                }
+
+            case 'help':
+                let roleStatus = msg.member.roles.has(role.id) ?
+                    `**Formas parte del rol ${roleName}**.` :
+                    `**No formas parte del rol ${roleName}**.`;
+                let helpMessages: string[] = [
+                    "Envía `!helper on` para unirte al grupo helper.",
+                    "Envía `!helper off` para abandonar el grupo helper.",
+                    "",
+                    "Otros términos reconocidos como on: true, t, yes, y, on, enable, enabled, 1, +, si, s, aceptar, ok",
+                    "Otros términos reconocidos como off: false, f, no, n, off, disable, disabled, 0, -, cancelar, cancel"
+                ]
+                return msg.reply([roleStatus].concat(helpMessages).join('\n'));
+
+            case 'invalid':
+                return msg.reply("Parámetro desconocido. Envía `!helper` para ver la ayuda.");
         }
     }
+
+    private static opmode(mode: string): string {
+        mode = mode.toLowerCase().trim();
+        if (mode == '') {
+            return 'help';
+        } else if (HelperCommand.yes.has(mode)) {
+            return 'yes';
+        } else if (HelperCommand.no.has(mode)) {
+            return 'no';
+        } else {
+            return 'invalid';
+        }
+    }
+
+    private static yes = new Set([
+        'true', 't', 'yes', 'y', 'on', 'enable', 'enabled', '1', '+',
+        'si', 's', 'aceptar', 'ok'
+    ]);
+    private static no = new Set([
+        'false', 'f', 'no', 'n', 'off', 'disable', 'disabled', '0', '-',
+        'cancelar', 'cancel'
+    ]);
 }
