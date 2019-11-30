d366cfaaa931588d2562fd9f21c61a8778bc5189
Dani Rodríguez <dani@danirod.es>
2019-11-30 23:19:53 +0100

    refactor: extract database opening into module
    This commit removes the logic for opening the database file and making
    sure that the directory exists, into a separate file that can be
    imported, removing a lot of file opening logic from Makibot.ts.
    
    Additionally, mkdirp is removed as a direct dependency because the logic
    has been refactored so that it uses the fs.mkdir method, which already
    has a recursive option since Node 10.

--- src/Makibot.ts
+++ src/Makibot.ts
@@ -1,14 +1,11 @@
 import * as Commando from 'discord.js-commando';
-import * as sqlite from 'sqlite';
 import * as path from 'path';
-import * as fs from 'fs';
 import ConfigSchema from './ConfigSchema';
 import { Message } from 'discord.js';
 import PinService from './hooks/pin';
-import { config as XDG_CONFIG } from 'xdg-basedir';
-import * as mkdirp from 'mkdirp';
 import RosterService from './hooks/roster';
 import VerifyService from './hooks/verify';
+import { getDatabase } from './settings';
 
 export default class Makibot extends Commando.CommandoClient {
 
@@ -34,8 +31,7 @@ export default class Makibot extends Commando.CommandoClient {
         this.on('ready', () => console.log(`Logged in successfully as ${this.user.tag}.`));
 
         this.once('ready', () => {
-            this.settingsFile()
-                .then(file => sqlite.open(file))
+            getDatabase()
                 .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
                 .then(() => {
                     // Reload presence using database values.
@@ -56,26 +52,6 @@ export default class Makibot extends Commando.CommandoClient {
         });
 
         this.login(ConfigSchema.token);
-    }
-
-    private settingsFile(): Promise<string> {
-        // This is what we want to return.
-        let configFile = path.join(XDG_CONFIG, 'clank', 'settings.db');
-
-        // But, hold on! Because it may be that the parent directory does not exist.
-        let configDir = path.dirname(configFile);
-        return new Promise((resolve, reject) => {
-            if (!fs.existsSync(configDir)) {
-                // I told you! Let's fix this.
-                mkdirp(configDir, err => {
-                    if (err) reject(err);
-                    else resolve(configFile);
-                });
-            } else {
-                // Ok, just let it go.
-                resolve(configFile);
-            }
-        });
     }
 
     shutdown() {
---
+++ src/settings.ts
@@ -0,0 +1,44 @@
+import { stat, mkdir } from "fs";
+import { dirname, resolve } from "path";
+
+import { Database, open as sqliteOpen } from "sqlite";
+import { config as XDG_CONFIG } from "xdg-basedir";
+
+function assertDirectoryExists(dir: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    stat(dir, (statErr, stats) => {
+      if (statErr) {
+        if (statErr.code === "ENOENT") {
+          // The directory does not exist. This is a valid case.
+          mkdir(dir, { recursive: true }, mkdirErr => {
+            if (mkdirErr) {
+              reject(mkdirErr);
+            } else {
+              resolve();
+            }
+          });
+        } else {
+          // We don't know which kind of error this is.
+          reject(statErr);
+        }
+      } else {
+        if (stats.isDirectory()) {
+          resolve();
+        } else {
+          reject(`${dir} exists but it is not a directory`);
+        }
+      }
+    });
+  });
+}
+
+export async function getDatabase(): Promise<Database> {
+  // The final location where the database file will be created.
+  const dbFile = resolve(XDG_CONFIG, "clank", "settings.db");
+
+  // Need to make sure first that the file exists.
+  const pathToDbFile = dirname(dbFile);
+  await assertDirectoryExists(pathToDbFile);
+
+  return sqliteOpen(dbFile);
+}
