271c60a307df66514a98c4f3f75c4b7f842cdbaf
Dani Rodríguez <dani@danirod.es>
2019-11-30 23:24:27 +0100

    chore: format Makibot.ts
    Made a separate commit to keep a minimal diff in the commit history for
    the project.

--- src/Makibot.ts
+++ src/Makibot.ts
@@ -1,61 +1,61 @@
-import * as Commando from 'discord.js-commando';
-import * as path from 'path';
-import ConfigSchema from './ConfigSchema';
-import { Message } from 'discord.js';
-import PinService from './hooks/pin';
-import RosterService from './hooks/roster';
-import VerifyService from './hooks/verify';
-import { getDatabase } from './settings';
+import path from "path";
+
+import Commando from "discord.js-commando";
+
+import ConfigSchema from "./ConfigSchema";
+import { getDatabase } from "./settings";
+import PinService from "./hooks/pin";
+import RosterService from "./hooks/roster";
+import VerifyService from "./hooks/verify";
 
 export default class Makibot extends Commando.CommandoClient {
+  public constructor() {
+    super({
+      commandPrefix: "!",
+      owner: ConfigSchema.owner,
+      disableEveryone: true,
+      unknownCommandResponse: false,
+    });
 
-    public constructor() {
-        super({
-            commandPrefix: '!',
-            owner: ConfigSchema.owner,
-            disableEveryone: true,
-            unknownCommandResponse: false
-        });
+    this.registry.registerDefaultTypes();
+    this.registry.registerGroups([
+      ["admin", "Administración"],
+      ["fun", "Entretenimiento"],
+      ["utiles", "Utilidad"],
+    ]);
+    this.registry.registerCommandsIn({
+      dirname: path.join(__dirname, "commands"),
+      filter: /^([^\.].*)\.ts$/,
+    });
 
-        this.registry.registerDefaultTypes();
-        this.registry.registerGroups([
-            ['admin', 'Administración'],
-            ['fun', 'Entretenimiento'],
-            ['utiles', 'Utilidad']
-        ]);
-        this.registry.registerCommandsIn({
-            dirname: path.join(__dirname, 'commands'),
-            filter: /^([^\.].*)\.ts$/
-        });
+    this.on("ready", () => console.log(`Logged in successfully as ${this.user.tag}.`));
 
-        this.on('ready', () => console.log(`Logged in successfully as ${this.user.tag}.`));
+    this.once("ready", () => {
+      getDatabase()
+        .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
+        .then(() => {
+          // Reload presence using database values.
+          this.user.setPresence({
+            status: this.settings.get("BotPresence", "online"),
+            game: { name: this.settings.get("BotActivity", null) },
+          });
 
-        this.once('ready', () => {
-            getDatabase()
-                .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
-                .then(() => {
-                    // Reload presence using database values.
-                    this.user.setPresence({
-                        status: this.settings.get('BotPresence', 'online'),
-                        game: { name: this.settings.get('BotActivity', null) }
-                    });
+          // Register hooks.
+          var pin = new PinService(this);
+          var roster = new RosterService(this);
 
-                    // Register hooks.
-                    var pin = new PinService(this);
-                    var roster = new RosterService(this);
+          if (process.env.VERIFY_CHANNEL && process.env.VERIFY_ROLE) {
+            var verify = new VerifyService(this);
+          }
+        })
+        .catch(console.log);
+    });
 
-                    if (process.env.VERIFY_CHANNEL && process.env.VERIFY_ROLE) {
-                        var verify = new VerifyService(this);
-                    }
-                })
-                .catch(console.log);
-        });
+    this.login(ConfigSchema.token);
+  }
 
-        this.login(ConfigSchema.token);
-    }
-
-    shutdown() {
-        console.log('The bot was asked to shutdown. Good night!');
-        this.destroy();
-    }
+  shutdown() {
+    console.log("The bot was asked to shutdown. Good night!");
+    this.destroy();
+  }
 }
