6e63a78e8f1f37df4d3511f41c6b2861ae45c0ce
Dani Rodríguez <dani@danirod.es>
2019-11-30 23:28:39 +0100

    refactor: cleanup database opening logic (#43)
    This commit removes the logic for opening the database file and making
    sure that the directory exists, into a separate file that can be
    imported, removing a lot of file opening logic from Makibot.ts.
    
    Additionally, mkdirp is removed as a direct dependency because the logic
    has been refactored so that it uses the fs.mkdir method, which already
    has a recursive option since Node 10.

--- src/Makibot.ts
+++ src/Makibot.ts
@@ -1,85 +1,61 @@
-import * as Commando from 'discord.js-commando';
-import * as sqlite from 'sqlite';
-import * as path from 'path';
-import * as fs from 'fs';
-import ConfigSchema from './ConfigSchema';
-import { Message } from 'discord.js';
-import PinService from './hooks/pin';
-import { config as XDG_CONFIG } from 'xdg-basedir';
-import * as mkdirp from 'mkdirp';
-import RosterService from './hooks/roster';
-import VerifyService from './hooks/verify';
+import path from "path";
+
+import Commando from "discord.js-commando";
+
+import ConfigSchema from "./ConfigSchema";
+import { getDatabase } from "./settings";
+import PinService from "./hooks/pin";
+import RosterService from "./hooks/roster";
+import VerifyService from "./hooks/verify";
 
 export default class Makibot extends Commando.CommandoClient {
+  public constructor() {
+    super({
+      commandPrefix: "!",
+      owner: ConfigSchema.owner,
+      disableEveryone: true,
+      unknownCommandResponse: false,
+    });
 
-    public constructor() {
-        super({
-            commandPrefix: '!',
-            owner: ConfigSchema.owner,
-            disableEveryone: true,
-            unknownCommandResponse: false
-        });
+    this.registry.registerDefaultTypes();
+    this.registry.registerGroups([
+      ["admin", "Administración"],
+      ["fun", "Entretenimiento"],
+      ["utiles", "Utilidad"],
+    ]);
+    this.registry.registerCommandsIn({
+      dirname: path.join(__dirname, "commands"),
+      filter: /^([^\.].*)\.ts$/,
+    });
 
-        this.registry.registerDefaultTypes();
-        this.registry.registerGroups([
-            ['admin', 'Administración'],
-            ['fun', 'Entretenimiento'],
-            ['utiles', 'Utilidad']
-        ]);
-        this.registry.registerCommandsIn({
-            dirname: path.join(__dirname, 'commands'),
-            filter: /^([^\.].*)\.ts$/
-        });
+    this.on("ready", () => console.log(`Logged in successfully as ${this.user.tag}.`));
 
-        this.on('ready', () => console.log(`Logged in successfully as ${this.user.tag}.`));
+    this.once("ready", () => {
+      getDatabase()
+        .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
+        .then(() => {
+          // Reload presence using database values.
+          this.user.setPresence({
+            status: this.settings.get("BotPresence", "online"),
+            game: { name: this.settings.get("BotActivity", null) },
+          });
 
-        this.once('ready', () => {
-            this.settingsFile()
-                .then(file => sqlite.open(file))
-                .then(db => this.setProvider(new Commando.SQLiteProvider(db)))
-                .then(() => {
-                    // Reload presence using database values.
-                    this.user.setPresence({
-                        status: this.settings.get('BotPresence', 'online'),
-                        game: { name: this.settings.get('BotActivity', null) }
-                    });
+          // Register hooks.
+          var pin = new PinService(this);
+          var roster = new RosterService(this);
 
-                    // Register hooks.
-                    var pin = new PinService(this);
-                    var roster = new RosterService(this);
+          if (process.env.VERIFY_CHANNEL && process.env.VERIFY_ROLE) {
+            var verify = new VerifyService(this);
+          }
+        })
+        .catch(console.log);
+    });
 
-                    if (process.env.VERIFY_CHANNEL && process.env.VERIFY_ROLE) {
-                        var verify = new VerifyService(this);
-                    }
-                })
-                .catch(console.log);
-        });
+    this.login(ConfigSchema.token);
+  }
 
-        this.login(ConfigSchema.token);
-    }
-
-    private settingsFile(): Promise<string> {
-        // This is what we want to return.
-        let configFile = path.join(XDG_CONFIG, 'clank', 'settings.db');
-
-        // But, hold on! Because it may be that the parent directory does not exist.
-        let configDir = path.dirname(configFile);
-        return new Promise((resolve, reject) => {
-            if (!fs.existsSync(configDir)) {
-                // I told you! Let's fix this.
-                mkdirp(configDir, err => {
-                    if (err) reject(err);
-                    else resolve(configFile);
-                });
-            } else {
-                // Ok, just let it go.
-                resolve(configFile);
-            }
-        });
-    }
-
-    shutdown() {
-        console.log('The bot was asked to shutdown. Good night!');
-        this.destroy();
-    }
+  shutdown() {
+    console.log("The bot was asked to shutdown. Good night!");
+    this.destroy();
+  }
 }
