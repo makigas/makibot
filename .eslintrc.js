137a3807311033867ab9834eba356385b49b71b5
Dani Rodríguez <dani@danirod.es>
2020-04-26 14:44:42 +0200

    fix: add @typescript-eslint/recommended too

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
   ],
   globals: {
     Atomics: "readonly",
