0bfc498b5bf238f657d23baddfb769b8fadb3951
Dani Rodríguez <dani@danirod.es>
2021-05-23 14:30:45 +0200

    fix: wrong evaluation of undefined variable
    Apparently undefined !== "" will return true.
    
    Testing for if (var) already returns false when var is undefined or var
    is "" (empty string), so worth. I don't know how JavaScript works.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ import { homedir } from "os";
 import { join } from "path";
 
 export function getConfigDirectory() {
-  if (process.env.XDG_CONFIG_HOME != "") {
+  if (process.env.XDG_CONFIG_HOME) {
     return process.env.XDG_CONFIG_HOME;
   } else {
     return join(homedir(), ".config");
