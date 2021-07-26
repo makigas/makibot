7eea854f37cde52132dbbd75fed0b13d8abc0253
Dani Rodríguez <dani@danirod.es>
2021-07-26 23:27:13 +0200

    refactor: use the new message quote in pins
    This commit will change the behaviour of the pin hook to use the new
    message quoter that was added in my previous commit. Unnecessary code
    has been removed.
    
    Additionally, this commit will fix an issue where reactions made in a
    news group would not be sent to the pinboard channel, since the channel
    of those messages is not of kind 'text' but 'news'.

--- src/lib/message.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js";
-
-export function getURL(message: Message): string {
-  const server = message.guild.id;
-  const channel = message.channel.id;
-  const post = message.id;
-  return `https://discordapp.com/channels/${server}/${channel}/${post}`;
-}
