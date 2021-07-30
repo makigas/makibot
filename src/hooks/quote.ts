c36aa056d6b7fc3ffcb105dec2040c10ce456267
Dani Rodríguez <dani@danirod.es>
2021-07-30 19:38:20 +0200

    feat: detect message permalinks and embed them
    This commit adds QuoteService. The QuoteService will detect messages
    containing permalinks pointing to a message of the same server. If
    found, the bot will attempt to build an embed with the contents of the
    linked message and send it to the channel as a quote.
    
    Closes #383

---
+++ src/hooks/quote.ts
@@ -0,0 +1,48 @@
+import { NewsChannel, TextChannel } from "discord.js";
+import { Hook } from "../lib/hook";
+import logger from "../lib/logger";
+import { quoteMessage } from "../lib/response";
+import Makibot from "../Makibot";
+
+function permalink(content: string) {
+  const regex = /discord\.com\/channels\/([0-9]+)\/([0-9]+)\/([0-9]+)/;
+  return content.match(regex);
+}
+
+export default class QuoteService implements Hook {
+  name = "quote";
+
+  constructor(private client: Makibot) {
+    this.client.on("message", async (originalMessage) => {
+      const ids = permalink(originalMessage.cleanContent);
+      if (ids) {
+        try {
+          const [_, guildId, channelId, messageId] = ids;
+          if (originalMessage.guild || originalMessage.guild.id === guildId) {
+            const channel = originalMessage.guild.channels.cache.get(channelId);
+            if (channel) {
+              switch (channel.type) {
+                case "text": {
+                  const message = await (channel as TextChannel).messages.fetch(messageId);
+                  if (message) {
+                    originalMessage.channel.send(quoteMessage(message));
+                  }
+                  break;
+                }
+                case "news": {
+                  const message = await (channel as NewsChannel).messages.fetch(messageId);
+                  if (message) {
+                    originalMessage.channel.send(quoteMessage(message));
+                  }
+                  break;
+                }
+              }
+            }
+          }
+        } catch (e) {
+          logger.error("[quote] cannot send message", e);
+        }
+      }
+    });
+  }
+}
