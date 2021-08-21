a5afde3f496ab5f78d144e9cf33ea9ea7e8b33dd
Dani Rodríguez <dani@danirod.es>
2021-08-21 21:44:39 +0200

    feat(antispam): add more links to be caught
    For some reason some people really likes to send links to Google Docs
    and Google Forms when they want people to fill surveys. Nothing against
    surveys, but get away from here with that stuff.
    
    Also, catch social links to Steam.

--- src/hooks/antispam.ts
+++ src/hooks/antispam.ts
@@ -20,7 +20,18 @@ const ruleset: { [reason: string]: RegExp[] } = {
     /facebook.com\/groups\/[\w._]+/, // intentionally also capture permalinks for posts in the group
     /facebook.com\/pages\/[\w._]+\/\d+/,
     /twitter.com\/\w+\/?(\?.+)?$/, // intentionally allow status because sharing tweets is common
-    /twitch.tv\/\w+/, // Twitch pages
+    /twitch.tv\/\w+/, // Twitch pages,
+    /steamcommunity.com\/id\/\w+/, // Steam profile pages by vanity URL.
+    /steamcommunity.com\/profiles\/\w+/, // Steam profile pages by ID.
+  ],
+  "El enlace apunta a una tienda de aplicaciones": [
+    /play.google.com\/store\/apps\/details/,
+  ],
+  "El enlace apunta a un Google Doc": [
+    /docs.google.com\/document\/d\//, // Google Docs
+    /docs.google.com\/spreadsheets\/d\//, // Google Docs
+    /docs.google.com\/presentations\/d\//, // Google Docs
+    /docs.google.com\/forms\/d\//, // Google Forms
   ],
 };
 
