e17d18c5b4c79a24d1d40f530a56fc5387b0c460
Dani Rodríguez <dani@danirod.es>
2022-02-25 21:18:17 +0100

    fix: mods cannot close a thread with /archivar
    They should be able because after all they are mods. They can use the
    regular actions and right clic a message, but this makes the /archivar
    command more useful to them.
    
    Fixes https://github.com/makigas/makibot/issues/503
    
    Thank you, matiuri.

--- src/interactions/commands/archivar.ts
+++ src/interactions/commands/archivar.ts
@@ -1,6 +1,7 @@
 import { CommandInteraction } from "discord.js";
 import { CommandInteractionHandler } from "../../lib/interaction";
 import { createToast } from "../../lib/response";
+import Server from "../../lib/server";
 
 /**
  * Archives a thread, as long as the thread was opened by me, or the thread was
@@ -13,8 +14,7 @@ export default class ArchivarCommand implements CommandInteractionHandler {
 
   async handle(event: CommandInteraction): Promise<void> {
     if (event.channel.isThread()) {
-      const message = await event.channel.fetchStarterMessage();
-      if (event.channel.ownerId === event.user.id || event.user.id === message.author.id) {
+      if (await canCloseMessage(event)) {
         await event.reply({ embeds: [ARCHIVED], ephemeral: true });
         await event.channel.setArchived(true, "Pedido mediante el comando /archivar");
       } else {
@@ -24,6 +24,41 @@ export default class ArchivarCommand implements CommandInteractionHandler {
       return event.reply({ embeds: [EXCUSE_NON_THREAD], ephemeral: true });
     }
   }
+}
+
+async function canCloseMessage(event: CommandInteraction) {
+  /*
+   * Rules that indicate whether a message can be closed or not. You can close
+   * the message if:
+   * - You are the person that opened the thread. (Because it is yours.)
+   * - You are the person that sent the original message. (Because it is your
+   *   message.)
+   * - You are mod.
+   */
+  if (event.channel.isThread()) {
+    /* You are the person that opened the thread. */
+    if (event.channel.ownerId === event.user.id) {
+      return true;
+    }
+
+    const message = await event.channel.fetchStarterMessage();
+
+    /* You are the person that sent the original message. */
+    if (message.author.id === event.user.id) {
+      return true;
+    }
+
+    const server = new Server(message.guild);
+    const member = await server.member(event.user.id);
+
+    /* You are the moderator of this server. */
+    if (member.moderator) {
+      return true;
+    }
+  }
+
+  /* Okay I tried. */
+  return false;
 }
 
 const ARCHIVED = createToast({
